dbb84a99d0de754a7f601d570f89d205db43122d
domifar <135108965+domifar@users.noreply.github.com>
2025-02-06 15:47:14 +0100

    lroe

--- Frontend/website_react-app/src/components/Home.jsx
+++ Frontend/website_react-app/src/components/Home.jsx
@@ -1,7 +1,28 @@
+import "../css/Home.css"
+import icon from "/main-icon.png"
+
 const Home = () => {
     return(
         <div id="home">
-            HOME
+            <div id="datatabs">
+                <div className="datatab">
+                    <div className="datatabtext">
+                        IP-Adresse: asdf{}
+                    </div>
+                    <div className="datatabtext">
+                        Name: asdf{}
+                    </div>
+                </div>
+                <div className="datatab">
+                    <div className="datatabtext">
+                        Gefahrene Strecke: asdf{}
+                    </div>
+                    <div className="datatabtext">
+                        Betriebsdauer: asdf{}
+                    </div>
+                </div>
+                <img id="homemainicon" src={icon} alt="MBot Icon" />
+            </div>
         </div>
     )
 }
